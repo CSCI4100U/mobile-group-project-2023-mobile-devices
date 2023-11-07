0591d77bfde9089cc4433938779ba7d01d15b25b
heirokos <ericericsouza02@gmail.com>
2023-11-07 18:05:56 -0500

    placeholder stats

--- elevar_fitness_tracker/lib/home_page/page_bodies/stats_body.dart
+++ elevar_fitness_tracker/lib/home_page/page_bodies/stats_body.dart
@@ -1,17 +1,83 @@
-/*
-  This file returns the encapsulating body widget for the Stats page
-*/
-
 import 'package:flutter/material.dart';
 import 'package:elevar_fitness_tracker/materials/styles.dart';
 
-Widget statsBody() {
+class statsBody extends StatelessWidget {
   AppStyles styles = AppStyles();
 
-  return Center(
-    child: Text(
-      "This is the Stats page",
-      style: styles.getHeadingStyle(),
-    ),
-  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Statistics",
+            style: styles.getHeadingStyle(styles.getObjectColor())),
+        backgroundColor: styles.getBackgroundColor(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '4 hrs this week',
+                style: styles.getSubHeadingStyle(styles.getObjectColor()),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  // Placeholder for the bar chart widget
+                  ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                  // Placeholder for the toggle buttons or chips for Duration, Volume, Reps
+                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  'Max Daily Volume:',
+                  style: styles.getMainTextStyle(),
+                ),
+                trailing: Text(
+                  '24385 Lbs',
+                  style: styles.getMainTextStyle(styles.getObjectColor()),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  'Max Daily Reps:',
+                  style: styles.getMainTextStyle(),
+                ),
+                trailing: Text(
+                  '972 Reps',
+                  style: styles.getMainTextStyle(styles.getObjectColor()),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  'Heaviest Lift:',
+                  style: styles.getMainTextStyle(),
+                ),
+                trailing: Text(
+                  'Hack Squat - 650 Lbs',
+                  style: styles.getMainTextStyle(styles.getObjectColor()),
+                ),
+              ),
+            ),
+            // more widgets as per needed etc
+          ],
+        ),
+      ),
+    );
+  }
 }
