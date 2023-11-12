edb150e3d326afc2c3c60a5d72afaf47e85d9add
D-Rosedo <danecathlyn.rosedo@ontariotechu.net>
2023-11-12 10:06:51 -0500

    out of app notifs, notif page

--- elevar_fitness_tracker/lib/home_page/page_bodies/home_body.dart
+++ elevar_fitness_tracker/lib/home_page/page_bodies/home_body.dart
@@ -4,7 +4,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:elevar_fitness_tracker/materials/styles.dart';
+<<<<<<< Updated upstream
 import 'package:pedometer/pedometer.dart';
+=======
+import 'package:path/path.dart';
+import 'package:pedometer/pedometer.dart';
+import 'package:elevar_fitness_tracker/notifications/notifications.dart';
+import 'package:elevar_fitness_tracker/notifications/notifPage.dart';
+>>>>>>> Stashed changes
 
 class homeBody extends StatefulWidget {
   homeBody({super.key});
@@ -15,6 +22,15 @@ class homeBody extends StatefulWidget {
 class _HomeBodyState extends State<homeBody> {
   AppStyles styles = AppStyles();
 
+<<<<<<< Updated upstream
+=======
+  final notifications = Notifications();
+  String notifTitle = "Elevar";
+  String notifBody =
+      "Welcome to Elevar! You have agreed to receive notifications for your workouts.";
+  String? payload;
+
+>>>>>>> Stashed changes
   late Stream<StepCount> _stepCountStream;
   String _stepCount = "0";
 
@@ -43,19 +59,61 @@ class _HomeBodyState extends State<homeBody> {
     _stepCountStream.listen(_trackStepCount).onError(_trackStepError);
   }
 
+<<<<<<< Updated upstream
   @override
   Widget build(BuildContext context) {
+=======
+  void notificationIconChange() {
+    setState(() {
+      currentNotifIcon = (currentNotifIcon == CupertinoIcons.bell)
+          ? CupertinoIcons.bell_slash
+          : CupertinoIcons.bell;
+    });
+  }
+
+  void sendNotification() {
+    notifications.sendNotification(
+        notifTitle, notifBody, "This is the payload");
+  }
+
+  void handleNotifButton() {
+    if (currentNotifIcon == CupertinoIcons.bell_slash) {
+      notificationIconChange();
+      sendNotification();
+    } else if (currentNotifIcon == CupertinoIcons.bell) {
+      notificationIconChange();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    notifications.init();
+
+>>>>>>> Stashed changes
     return Scaffold(
       appBar: AppBar(
         title: Text("Home", style: styles.getHeadingStyle()),
         backgroundColor: styles.getHighlightColor(),
         actions: [
           //in-app/local notifs?
+<<<<<<< Updated upstream
           IconButton(
             icon: Icon(CupertinoIcons.bell),
             onPressed: (() {
               notificationIconChange();
             }),
+=======
+          ElevatedButton(
+            child: Icon(currentNotifIcon),
+            onPressed: handleNotifButton,
+            onLongPress: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: ((context) => NotifPage()),
+                  ));
+            },
+>>>>>>> Stashed changes
           )
         ],
       ),
@@ -63,8 +121,12 @@ class _HomeBodyState extends State<homeBody> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+<<<<<<< Updated upstream
           // First Container with Stack
           Container(
+=======
+          SizedBox(
+>>>>>>> Stashed changes
             width: 150.0,
             height: 150.0,
             child: Stack(
@@ -74,7 +136,11 @@ class _HomeBodyState extends State<homeBody> {
                   width: 150.0,
                   height: 150.0,
                   decoration: BoxDecoration(
+<<<<<<< Updated upstream
                     color: Color(0xFF00CCFF),
+=======
+                    color: const Color(0xFF00CCFF),
+>>>>>>> Stashed changes
                     borderRadius: BorderRadius.circular(100.0),
                   ),
                 ),
@@ -86,12 +152,20 @@ class _HomeBodyState extends State<homeBody> {
               ],
             ),
           ),
+<<<<<<< Updated upstream
           SizedBox(height: 20.0), // Add some space between containers
+=======
+          const SizedBox(height: 20.0), // Add some space between containers
+>>>>>>> Stashed changes
 
           Container(
             width: 300.0,
             height: 150.0,
+<<<<<<< Updated upstream
             margin: EdgeInsets.all(20.0),
+=======
+            margin: styles.getDefaultInsets(),
+>>>>>>> Stashed changes
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20.0),
               border: Border.all(color: Colors.black, width: 2.0),
@@ -109,6 +183,7 @@ class _HomeBodyState extends State<homeBody> {
       )),
     );
   }
+<<<<<<< Updated upstream
 
   void notificationIconChange() {
     setState(() {
@@ -117,4 +192,6 @@ class _HomeBodyState extends State<homeBody> {
           : CupertinoIcons.bell;
     });
   }
+=======
+>>>>>>> Stashed changes
 }
---
+++ elevar_fitness_tracker/lib/notifications/notifPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:elevar_fitness_tracker/materials/styles.dart';
+
+class NotifPage extends StatefulWidget {
+  const NotifPage({super.key});
+
+  @override
+  State<NotifPage> createState() => _NotifPageState();
+}
+
+class _NotifPageState extends State<NotifPage> {
+  AppStyles styles = AppStyles();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notifications", style: styles.getHeadingStyle()),
+        backgroundColor: styles.getHighlightColor(),
+      ),
+      body: ListView(children: [
+        ListTile(
+          title: Text(''),
+        )
+      ]),
+    );
+  }
+}
