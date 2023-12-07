a9287652849bf93d1305f8bac18ad425faec9970
heirokos <ericericsouza02@gmail.com>
2023-12-06 23:30:21 -0500

    get data from the routines

--- elevar_fitness_tracker/lib/home_page/page_bodies/stats_body.dart
+++ elevar_fitness_tracker/lib/home_page/page_bodies/stats_body.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:elevar_fitness_tracker/materials/styles.dart';
-import 'package:elevar_fitness_tracker/local_storage/exercises_db_model.dart';
+import 'package:elevar_fitness_tracker/local_storage/routine_db_model.dart';
 
 class statsBody extends StatelessWidget {
-  final ExerciseDBModel dbModel = ExerciseDBModel();
+  final AppStyles styles = AppStyles();
+  final RoutineDBModel dbModel = RoutineDBModel();
   bool darkmode = false;
 
   @override
@@ -11,36 +12,35 @@ class statsBody extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppStyles.primaryColor(darkmode).withOpacity(0.2),
       appBar: AppBar(
-        title: Text("Your Statistics",
-            style: AppStyles.getHeadingStyle(darkmode)),
+        title:
+            Text("Your Statistics", style: AppStyles.getHeadingStyle(darkmode)),
         backgroundColor: AppStyles.primaryColor(darkmode),
       ),
       body: FutureBuilder<List<Map<String, dynamic>>>(
-        // get the max stats for each exercise
-        future: dbModel.getMaxStatsForAllExercises(),
+        future: dbModel.getMaxStatsForEachExerciseInRoutines(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-            List<Map<String, dynamic>> maxStats = snapshot.data!;
+            List<Map<String, dynamic>> exerciseStats = snapshot.data!;
             return ListView.builder(
-              itemCount: maxStats.length,
+              itemCount: exerciseStats.length,
               itemBuilder: (context, index) {
-                var exercise = maxStats[index];
+                var exerciseStat = exerciseStats[index];
                 return ListTile(
-                  title:
-                      Text(exercise['name'], style: AppStyles.getSubHeadingStyle(darkmode)),
+                  title: Text(exerciseStat['exerciseName'],
+                      style: AppStyles.getSubHeadingStyle(darkmode)),
                   subtitle: Text(
-                    'Max Reps: ${exercise['maxReps']} | Max Weight: ${exercise['maxWeight']} lbs',
+                    'Max Reps: ${exerciseStat['maxReps']} | Max Weight: ${exerciseStat['maxWeight']} lbs',
                     style: AppStyles.getMainTextStyle(darkmode),
                   ),
                 );
               },
             );
           } else {
-            return const Center(child: Text('No exercise data available'));
+            return const Center(child: Text('No exercise stats available'));
           }
         },
       ),
